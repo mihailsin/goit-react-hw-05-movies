d65f3008f97d11200e3bc0dc84d79ed5882a0678
mihailsin <mihailsin88@gmail.com>
2022-02-04 13:50:24 +0200

    adds lazy import

--- src/App.js
+++ src/App.js
@@ -1,24 +1,41 @@
+import { lazy, Suspense } from 'react';
 import { Route, Routes, Navigate } from 'react-router-dom';
 import './App.css';
-import Navigation from './components/Navigation';
-import HomePage from './pages/HomePage';
-import MoviesPage from './pages/MoviesPage';
-import MovieDetails from './components/MovieDetails';
-import MovieCast from './components/MovieCast';
-import MovieReviews from './components/MovieReviews';
+
+const HomePage = lazy(() =>
+  import('./pages/HomePage' /*webpackChunkName: "Home"*/)
+);
+const MoviesPage = lazy(() =>
+  import('./pages/MoviesPage' /*webpackChunkName: "Movies"*/)
+);
+const MovieDetails = lazy(() =>
+  import('./components/MovieDetails' /*webpackChunkName: "MovieDetails"*/)
+);
+const Navigation = lazy(() =>
+  import('./components/Navigation' /*webpackChunkName: "Nav"*/)
+);
+const MovieCast = lazy(() =>
+  import('./components/MovieCast' /*webpackChunkName: "Cast"*/)
+);
+const MovieReviews = lazy(() =>
+  import('./components/MovieReviews' /*webpackChunkName: "Reviews"*/)
+);
+
 function App() {
   return (
     <div>
-      <Navigation />
-      <Routes>
-        <Route path="/" element={<HomePage />} />
-        <Route path="/movies" element={<MoviesPage />} />
-        <Route path="/movies/:movieId/*" element={<MovieDetails />}>
-          <Route path="cast" element={<MovieCast />} />
-          <Route path="reviews" element={<MovieReviews />} />
-        </Route>
-        <Route path="*" element={<Navigate to="/" />} />
-      </Routes>
+      <Suspense fallback={<h1>Loading...</h1>}>
+        <Navigation />
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/movies" element={<MoviesPage />} />
+          <Route path="/movies/:movieId/*" element={<MovieDetails />}>
+            <Route path="cast" element={<MovieCast />} />
+            <Route path="reviews" element={<MovieReviews />} />
+          </Route>
+          <Route path="*" element={<Navigate to="/" />} />
+        </Routes>
+      </Suspense>
     </div>
   );
 }
--- src/components/MoviesSearch/MoviesSearch.jsx
+++ src/components/MoviesSearch/MoviesSearch.jsx
@@ -1,33 +1,16 @@
 import { useState, useEffect } from 'react';
-import {
-  Link,
-  useLocation,
-  useNavigate,
-  useSearchParams,
-} from 'react-router-dom';
+import { Link, useSearchParams } from 'react-router-dom';
 import getMovies from '../../services/theMovieDbApi';
-// const queryString = require('query-string');
+
 const MoviesSearch = () => {
-  // const [searchQuery, setSearchQuery] = useState('');
   const [movies, setMovies] = useState([]);
-  // const location = useLocation();
-  // const navigate = useNavigate();
+
   const [searchParams, setSearchParams] = useSearchParams();
-
-  // const queryParams = queryString.parse(location.search);
-  // const queryValue = Object.values(queryParams).toString();
-
-  // console.log(queryValue);
-  // console.log(navigate);
-  // console.log(location);
 
   const onSubmit = e => {
     e.preventDefault();
 
-    // setSearchQuery(e.target.searchInput.value);
-    // navigate(`/movies?query=${e.target.searchInput.value}`);
     setSearchParams({ query: e.target.searchInput.value });
-
     console.log('searchParams:', searchParams.get('query'));
 
     e.target.reset();
